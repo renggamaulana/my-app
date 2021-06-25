9dbd18e3a7a202bb8e4a3ff5b27deb8c562bbac8
Rengga Maulana <renggaa21@gmail.com>
2021-06-25 08:49:05 +0700

    initial commit

---
+++ src/Components/Navbar.svelte
@@ -0,0 +1,32 @@
+<nav>
+  <ul class="nav-link">
+    <li><a href="#home">Home</a></li>
+    <li><a href="#home">About</a></li>
+    <li><a href="#home">Contact</a></li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    height: 10vh;
+    width: 100%;
+    background-color: rgb(48, 216, 216);
+  }
+  .nav-link {
+    list-style: none;
+    text-decoration: none;
+    display: flex;
+    justify-content: space-around;
+    width: 50%;
+    height: 100%;
+    align-items: center;
+    margin-left: auto;
+  }
+
+  .nav-link li a {
+    text-decoration: none;
+    justify-content: space-around;
+    color: white;
+    font-size: 18px;
+  }
+</style>
