45d2152a88e2e371dcf76a474e5e19cc036ef5b2
Rengga Maulana <renggaa21@gmail.com>
2021-07-16 07:47:50 +0700

    membuat migration pada Backend

--- Frontend/src/Components/Admin/Menu.svelte
+++ Frontend/src/Components/Admin/Menu.svelte
@@ -19,11 +19,11 @@
         <img
           src="dist/img/user2-160x160.jpg"
           class="img-circle elevation-2"
-          alt="User Image"
+          alt="User"
         />
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="profile" class="d-block">Alexander Pierce</a>
       </div>
     </div>
 
